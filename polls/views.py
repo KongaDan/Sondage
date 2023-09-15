2a54cb423fea8fd01795ee724e227d8805c81d6f
konga <kongadan68@gmail.com>
2023-09-15 13:35:20 +0200

    fichiers statique

--- polls/views.py
+++ polls/views.py
@@ -5,6 +5,7 @@ from .models import Question, Choice
 from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
 from django.views import generic 
+from django.utils import timezone
 #from django.http import Http404
 """
 def index(request):
@@ -96,6 +97,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model=Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
     model=Question
